43367073284f13f5810e6275682a0c09e390144c
Rishabh <chaudharyrishabh0210@gmail.com>
2023-07-08 14:14:57 +0530

    added responsiveNess for main page

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 let fCard = document.querySelectorAll('.f-card');
+let burger = document.querySelector(".burger");
+let nav = document.querySelector("nav");
+let navLinks = document.querySelectorAll("nav>.navbar>a");
 
 let j = 0;
 
@@ -11,3 +14,15 @@ setInterval(()=>{
     j = j+1;
     console.log("Hello world from j loop " + j);
 }, 3000)
+
+
+burger.addEventListener('click', ()=>{
+    burger.classList.toggle('active');
+    nav.classList.toggle("active");
+})
+
+navLinks.forEach(element => {
+    element.addEventListener('click', ()=>{
+        nav.classList.toggle("active");
+    })
+});
